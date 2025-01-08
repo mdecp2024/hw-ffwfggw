f012e6c937d27a6369977c76f4737039d6469c52
ffwfggw <41323216@nfu.edu.tw>
2025-01-08 01:41:30 +0000

    Final Report

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from gevent.pywsgi import WSGIServer
 
 #flaskapp.app.run(host="0.0.0.0", debug=True)
 http_server = WSGIServer(('0.0.0.0', 8081), flaskapp.app)
-http_server.serve_forever()<iframe width="560" height="315" 
+http_server.serve_forever()
